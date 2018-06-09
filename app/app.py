973c4d101ae885344949d7f6cd2a9787cc311510
Mateusz Marian Bajdak <m.bajdak@stud.elka.pw.edu.pl>
2018-06-09 02:49:45 +0200

    SLSQP minimization

--- app/app.py
+++ app/app.py
@@ -1,15 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import minimize
 
 if __name__ == '__main__':
-    x = np.arange(-12, 12, 0.1)
-    x1 = np.power(np.subtract(x, 6), 2)
-    x2 = np.power(x, 2)
+    x_space = np.linspace(-12, 12, 100)
+    x1 = np.power(np.subtract(x_space, 6), 2)
+    x2 = np.power(x_space, 2)
+
+    x1_prym = []
+    x2_prym = []
+
+    for alfa in np.arange(0, 1, 0.1):
+        result = minimize(
+            lambda x: alfa * (x[0] ** 2) + (1 - alfa) * ((x[0] - 6) ** 2),
+            np.array([0]),
+            method='SLSQP',
+            bounds=[(-12, 12,)]
+        )
+        x1_prym.append((result.x[0] - 6) ** 2)
+        x2_prym.append(result.x[0] ** 2)
 
     plt.figure(1)
-    plt.plot(x1, x2, 'b-')
+    plt.plot(x1, x2, 'b-', x1_prym, x2_prym, 'r*')
+    plt.axis([-5, 50, -5, 50])
     plt.xlabel('(x-6)^2')
     plt.ylabel('x^2')
     plt.grid(True)
-    plt.axis([-30, 60, -30, 100])
     plt.show()
