2c19e8e4c2357dc2e735918b54b83bb24c2de8aa
Mateusz Marian Bajdak <m.bajdak@stud.elka.pw.edu.pl>
2018-06-09 03:18:57 +0200

    ga added

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from platypus import NSGAII, Problem, Real, nondominated
 from scipy.optimize import minimize
 
 if __name__ == '__main__':
@@ -20,6 +21,28 @@ if __name__ == '__main__':
         x1_prym.append((result.x[0] - 6) ** 2)
         x2_prym.append(result.x[0] ** 2)
 
+    problem = Problem(1, 2)
+    problem.types[:] = Real(-12, 12)
+    nondominated_solutions = []
+
+
+    def shaffer(x):
+        return [x[0] ** 2, (x[0] - a - 1) ** 2]
+
+
+    problem.function = shaffer
+
+    for a in np.arange(0, 1, 0.1):
+        algorithm = NSGAII(problem)
+        algorithm.run(300)
+        nondominated_solutions.append(nondominated(algorithm.result))
+
+    for solutions in nondominated_solutions:
+        print('---------')
+        for solution in solutions:
+            print(solution.objectives)
+        print('---------')
+    
     plt.figure(1)
     plt.plot(x1, x2, 'b-', x1_prym, x2_prym, 'r*')
     plt.axis([-5, 50, -5, 50])
